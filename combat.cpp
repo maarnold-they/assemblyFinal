b0c7957ef592b1f764c713a36312f3cee9d951bb
anihortes <42783529+anihortes@users.noreply.github.com>
2019-11-23 18:00:14 -0900

    Add files via upload
    i cant properly sync for some reason so im manually uploading combat.cpp 
    i put important things in comments at the top of my file

--- combat.cpp
+++ combat.cpp
@@ -1,5 +1,6 @@
 #include "levelTemplate.h"
 #include <vector>
+#include <stdlib.h>     /* srand, rand */
 using std::cout;
 using std::cin;
 using std::endl;
@@ -7,36 +8,168 @@ using std::string;
 using std::vector;
 using std::pair;
 
-void temp(character & protag, character & enemy) {
-	while (protag.HP > 0 || enemy.HP > 0) {
-		cout << enemy.name << " wants to fight!\n What will you do?\n";
-		vector <pair<string, int>> options(3);
-		options[0].first = "give big slappy\n";
-		options[0].second = 1;
-		options[1].first = "protecc urself from hitt\n";
-		options[1].second = 2;
-		options[2].first = "idk what to type here 1\n";
-		options[2].second = 3;
-		options[3].first = "idk what to put here 2, electric boogaloo\n";
-		options[3].second = 4;
-		for (int i = 0; i <= options.size(); i++) {
-			cout << options[i].first;
-		}
-		int input;
-		cin >> input;
-		
-			if (input == options[i].second) {
-				cout << "you give the big slappy... oh you know his feelings hurt.\n";
+/*
+PUT IN HEADER FILE:
+	void fight(character& protag, character& enemy);
+
+TEST DUMMIES (just slap them into main):
+	character protag;
+	protag.name = "bobby";
+	protag.HP = 40;
+	character enemy;
+	enemy.name = "joey";
+	enemy.HP = 20;
+	fight(mainChar, enemy);
+*/
+
+void printOptions(vector <pair<string, string>> options) {
+	for (int i = 0; i < options.size(); i++) {
+		cout << options[i].first;
+	}
+	cout << "                         INPUT HERE: ";
+}
+
+vector <pair<string, string>> setOptions() {
+	vector <pair<string, string>> setOptions(4);
+	setOptions[0].first = "\ngive big slap (1)";
+	setOptions[0].second = "1";
+	setOptions[1].first = "\nprotecc urself from hitt (2)";
+	setOptions[1].second = "2";
+	setOptions[2].first = "\nfocus (3)";
+	setOptions[2].second = "3";
+	setOptions[3].first = "\nprint stats (4)\n\n";
+	setOptions[3].second = "4";
+	return setOptions;
+}
+
+/*/
+player input is compared with vector options in IF statements
+if bool guard = true, then character is guarding against players next attack
+*/
+void playerMove(vector <pair<string, string>> options, int& weapon, string input,
+	character& protag, character& enemy, bool& enemyGuard, bool& playerGuard) {
+	bool temp = true;
+	while (temp) {
+		if (input == options[0].second) {
+			if (enemyGuard) {
+				cout << "\nwhat can i say, the mans ready and your fists were\n";
+				cout << "a straw too short. no damage.\n";
 			}
 			else {
-				cout << "you idle around and he hits you!!! >:(\n";
-				if (enemy.HP > 20) {
-					cout << "oh boy he looks like hes not hurt, watcha gonna do bud?\n";
-					//getline(cin, input);
-				}
+				cout << "\nyou give the big slap... oh you know his feelings hurt.\n";
+				enemy.HP = enemy.HP - weapon;
 			}
-		
-		vector <pair<int, int>> options;
+			playerGuard = false;
+			temp = false;
+		}
+
+		else if (input == options[1].second) {
+			cout << "\nyou block\n";
+			playerGuard = true;
+			temp = false;
+		}
+
+		else if (input == options[2].second) {
+			if (weapon < 10) {
+				cout << "\nyou focus your mind and better your stance.\n";
+				cout << "\nDamage increased!\n";
+				weapon = weapon + 3;
+			}
+			else {
+				cout << "\nyou cannot power up any more than this.";
+			}
+			temp = false;
+		}
+
+		else if (input == options[3].second) {
+			cout << "\nPLAYER: " << protag.name << endl;
+			cout << "HEALTH: " << protag.HP << endl;
+			cout << "WEAPON DAMAGE: " << weapon << endl;
+			cout << "\nNPC: " << enemy.name << endl;
+			cout << "HEALTH: " << enemy.HP << endl;
+			printOptions(options);
+			getline(cin, input);
+		}
+		else {
+			cout << "\nERROR\n";
+			printOptions(options);
+			getline(cin, input);
+		}
+	}
+}
+
+/*
+enemy can do two things, attack or guard
+enemy is more likely to attack
+rand is called to determine which it does
+*/
+void enemyMove(vector <pair<string, string>> options,
+	character& protag, character& enemy, bool& enemyGuard, bool& playerGuard) {
+	int enemyMove = rand() % 3; //chooses a move between 0 and 2
+	if (enemyMove != 0) {
+		if (playerGuard) {
+			cout << "\nhe tried to hit! wow! nothing happened to you!\n";
+		}
+		else {
+			cout << "\nthe enemy slugs you good and bad. YEE-OUCH BABEY!!\n";
+			protag.HP = protag.HP - 5;
+		}
+		enemyGuard = false;
 	}
 
+	else {
+		cout << "\nthe man guards himself against the next attack\n";
+		enemyGuard = true;
+	}
+}
+
+/*
+every turn the enemies condition is displayed
+*/
+void fightHeader(character enemy) {
+	int hpChecker = 10; //reference for dialouge of enemies status
+	if (enemy.HP > hpChecker) {
+		cout << enemy.name << " wants to fight!\nWhat will you do?\n";
+	}
+	else if (hpChecker/2 < enemy.HP <= hpChecker){
+		cout << enemy.name << " looks tired!\nWhat will you do?\n";
+
+	}
+	else {
+		cout << enemy.name << " looks like they might collapse any moment!\n";
+		cout << "What will you do ? \n";
+	}
+}
+
+/*
+main function for combat. uses while loop for each turn
+*/
+void fight(character& protag, character& enemy) {
+	bool enemyGuard=false;
+	bool playerGuard=false;
+	vector <pair<string, string>> options = setOptions();		
+	int weapon = 5; //TEMP PLACEHOLDER, REPLACE WITH CHARACTER WEAPON DAMAGE LATER
+	while (protag.HP > 0 && enemy.HP > 0) {
+		fightHeader(enemy);
+		//prints full combat options for player
+		printOptions(options);
+		string input;
+		getline(cin, input);
+		playerMove(options, weapon, input, protag, enemy, enemyGuard, playerGuard);
+		
+		//checks enemies health in case they died before while loop checks HP
+		if (enemy.HP > 0) {
+			enemyMove(options, protag, enemy, enemyGuard, playerGuard);
+		}
+		//helps break each turn into sections
+		cout << "-------------------------------------------\n";
+	}
+
+	if (protag.HP == 0) {
+		cout << "\nyou died.\n";
+	}
+
+	else {
+		cout << "\nthe bad guy died yay. \n";
+	}
 }
