db415a6d40f13eb605c989374dab8fb47e98e4ee
Adam Owen <ahowenjr@alaska.edu>
2019-12-01 17:11:19 -0900

    added to lvl2

--- 301Project/301Project/level2.cpp
+++ 301Project/301Project/level2.cpp
@@ -15,6 +15,7 @@ cout << "" << endl;
 
 string lvl2Start(character& mainChar, void typo()) {
 	cout << "you go through the door and find yourself in a fancy looking apartment." << endl;
+	cout << "you see a bathroom to your left and what looks to be a dining room ahead of you."
 	cout << "You hear the gang members shouting growing closer."<< endl;
 	string choice;
 	while (true) {
@@ -39,6 +40,7 @@ string lvl2Start(character& mainChar, void typo()) {
 			cout << "You manage to defeat the thug, only to realize" << endl;
 			cout << "that there's, like, 15 other gang members making their way towards you." << endl;
 			cout << "You look to your left and notice a bathroom and run in." << endl;
+			lvl2bathroom(mainChar, typo());
 			break;
 		}
 		if (choice == "3") {
@@ -48,4 +50,35 @@ string lvl2Start(character& mainChar, void typo()) {
 		}
 		return "dummy";
 	}
+}
+
+void lvl2bathroom(character& mainChar, void typo()) {
+	int wait = 0; //tracks how long use has been in the bathroom. at wait=3 thugs leave the room
+	cout << "you quickly shut the bathroom door and lock it.\n";
+	cout << "it isnt until you hear the thugs walk past the bathroom that you take the time inspect the bathroom.\n";
+	string choice;
+	while (true) {
+		cout << "what do you do?\n";
+		cout << "(1) look for something useful                 ";
+		if (wait < 3) {
+			cout << "(2) try to surprise attack the thugs             (3) wait for the thugs to leave\n";
+		}
+		else {
+			cout << "leave the bathroom.\n";
+		}
+		getline(cin, choice);
+		cout << endl;
+		if (choice == "1")
+		{
+			cout << "you look around but cant find anything useful.\n";
+			if (wait < 3) {
+				cout << "you hear the thugs rummaging around the room.\n";
+					wait++;
+			}
+			else {
+				cout << "the room is silent and you can go without worry of attack.\n";
+			}
+		}
+	}
+
 }
