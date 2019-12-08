c427b5ced75a9b228ac0665c30e07836d1c4663a
Millard Arnold V <millard.arnold.v@gmail.com>
2019-12-07 15:06:57 -0900

    Added comments, and fixed inventory

--- 301Project/301Project/inventory.cpp
+++ 301Project/301Project/inventory.cpp
@@ -4,7 +4,13 @@
 //Dec. 7, 2019
 
 #include "levelTemplate.h"
-unsigned long long noWeapons = 0xFFff0000FFffFFff;
+
+const unsigned long long noWeapons = 0xFFff0000FFffFFff; //Constant designed to remove all the weapons so that only one weapon is held at a time.
+
+/*
+The new weapon function removes all weapons and then adds a new one, but the others just add a new item of the correct type by flipping the bits that correspond with it.
+Takes input of the item & character.
+*/
 void newWeapon(int weapon, character& protag)
 {
 	protag.inventory = (protag.inventory & noWeapons) | ((unsigned long long)weapon << 32);
@@ -17,15 +23,20 @@ void newKey(int key, character& protag)
 {
 	protag.inventory = protag.inventory | (key);
 }
+
+/*
+The check functions check to see if the correct bits are flipped to correspond to the specific item for each item type.
+Takes input of the item & character.
+*/
 bool checkWeapon(int weapon, character& protag)
 {
-	return weapon == (int)((protag.inventory >> 32) & 0xFFff);
+	return weapon == (int)((protag.inventory >> 32) & weapon);
 }
 bool checkUtility(int utility, character& protag)
 {
-	return utility == (int)(protag.inventory >> 48);
+	return utility == (int)((protag.inventory >> 48)&utility);
 }
 bool checkKey(int key, character& protag)
 {
-	return key == (int)(protag.inventory & 0xFFffFFff);
+	return key == (int)(protag.inventory & key);
 }
